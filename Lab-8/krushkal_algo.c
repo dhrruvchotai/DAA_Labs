b40fa20f319752de4cdcbe2e9c4293f0bb82bc51
dhrruvchotai <23010101051@darshan.ac.in>
2025-07-29 16:23:36 +0530

    added Lab-8

---
+++ Lab-8/krushkal_algo.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define V 4
+#define E 6
+
+struct Edge
+{
+    int source;
+    int dest;
+    int weight;
+};
+
+struct Edge edges[] = {
+    {0, 1, 2}, {1, 2, 1}, {2, 3, 4}, {3, 0, 10}, {1, 3, 8}, {0, 2, 5}};
+
+int parent[V];
+int mst[V - 1];
+
+
+int comp(struct Edge* a, struct Edge* b)
+{
+    return a->weight - b->weight;
+}
+
+int findParent(int parent[],int index){
+    if(parent[index] == index){
+        return index;
+    }
+    findParent(parent,parent[index]);
+}
+
+void unionSet(int ucomp, int vcomp){
+    parent[ucomp] = vcomp;
+}
+
+void main()
+{   
+    int totalWeight = 0;
+
+    for (int i = 0; i < V; i++)
+    {
+        parent[i] = i;
+    }
+
+    qsort(edges, sizeof(edges) / sizeof(edges[0]), sizeof(struct Edge), comp);
+
+    for (int i = 0; i < E; i++)
+    {
+        int ucomp = findParent(parent,edges[i].source);
+        int vcomp = findParent(parent,edges[i].dest);
+
+        if(ucomp != vcomp){
+            totalWeight += edges[i].weight;
+            unionSet(ucomp,vcomp);
+        }
+    }
+
+    printf("Total Weight Is : %d",totalWeight);
+}
