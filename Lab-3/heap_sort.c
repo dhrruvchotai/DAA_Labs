b1172d4b5cc0ee60bf4124e9b370679a7d96d532
DHRUV <154119792+dhrruvchotai@users.noreply.github.com>
2025-09-16 07:38:19 +0530

    Update heap_sort.c

--- Lab-3/heap_sort.c
+++ Lab-3/heap_sort.c
@@ -1,25 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void heapify(int arr[] ,int size, int i){
-    int largest = i;
-    int left = 2 * i + 1;
+void heapify(int arr[], int size, int i) {
+    int largest = i;        
+    int left = 2 * i + 1;    
+    int right = 2 * i + 2;   
 
+    if (left < size && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < size && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, size, largest);
+    }
 }
 
-void heapSort(int arr[], int size){
-    for(int i = size / 2 - 1 ; i >= 0 ; i--){
-        heapify(arr,size,i);
-    }
+void heapSort(int arr[], int size) {
+    for (int i = size / 2 - 1; i >= 0; i--)
+        heapify(arr, size, i);
 
-    for(int i = size - 1 ; i > 0 ; i--){
-        heapify(arr,i,0);
+    for (int i = size - 1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify(arr, i, 0);
     }
 }
 
-void main(){
-    int arr[] = {7,3,5,7,8,3,2,5};
-    heapify(arr,0);
-    for(int i = 0;i<8;i++){
-        printf("%d ",arr[i]);
-    }
+int main() {
+    int arr[] = {7, 3, 5, 7, 8, 3, 2, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    heapSort(arr, size);
+
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
 }
